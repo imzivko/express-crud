500dacde36a19bce72d017b53d70ffcfd3d969ed
Ivan Zivkovic <imzivko@gmail.com>
2022-10-12 14:23:46 +0200

    init

--- dist/setupFilesAfterEnv.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = require("./db");
-global.afterAll(async () => {
-    await db_1.client.close();
-});
-//# sourceMappingURL=setupFilesAfterEnv.js.map
